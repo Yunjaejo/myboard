b26a2b17c07f1056673ad3a71a686fedc284797b
yunjaejo <1a616@naver.com>
2021-09-24 23:34:01 +0900

    베이스

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express') // 익스프레스 참조
+const app = express() // 익스프레스 쓸때는 app이라고 명시
+const port = 3000
+
+// const connect = require("./schemas"); // schemas 폴더 참조
+// connect(); // 모델이랑 연결하기
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json()) // POST로 메소드 받을 때 req.body로 사용가능하게 함
+
+app.set('views', __dirname + '/views'); // views의 경로는 /views
+app.set('view engine', 'ejs'); // 뷰 엔진으로 ejs 사용
+
+app.get('/write', (req, res) => {
+    res.render('write')
+})
+
+app.get('/list', (req, res) => {
+    res.render('list')
+})
+
+app.get('/edit', (req, res) => {
+    res.render('edit')
+})
+
+app.get('/detail', (req, res) => {
+    res.render('detail')
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`)
+})
