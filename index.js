a7ebf61df7a2c09dc59f3b3547cd2c56b748ef68
yunjaejo <1a616@naver.com>
2021-09-25 17:04:09 +0900

    write ajax

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express') // 익스프레스 참조
 const app = express() // 익스프레스 쓸때는 app이라고 명시
 const port = 3000
+const morgan = require('morgan')
+
+app.use(morgan('dev')) // 요청과 주소, 에러 기록
 
 const connect = require("./schemas"); // schemas 폴더 참조
 connect(); // 모델이랑 연결하기
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json()) // POST로 메소드 받을 때 req.body로 사용가능하게 함
+
+const postRouter = require("./routers/post"); // routes아님 routers임
+app.use("/api", [postRouter]); // postRouter를 api 하위부분에서 쓰겠다 !
 
 app.set('views', __dirname + '/views'); // views의 경로는 /views
 app.set('view engine', 'ejs'); // 뷰 엔진으로 ejs 사용
 
 app.get('/write', (req, res) => {
     res.render('write')
-    console.log(req.body)
 })
 
 app.get('/list', (req, res) => {
@@ -26,6 +31,11 @@ app.get('/edit', (req, res) => {
 
 app.get('/detail', (req, res) => {
     res.render('detail')
+})
+
+app.post('/postTest', (req, res) => {
+    console.log(req.body)
+    res.json({ ok: true })
 })
 
 
--- routers/post.js
+++ routers/post.js
@@ -3,21 +3,13 @@ const Post = require("../schemas/Post"); // Goods라는 스키마 참조
 
 const router = express.Router(); // 익스프레스 라우터를 router로 쓰겠다 
 
-router.get("/post", async (req, res, next) => {
-    try {
-        const { category } = req.query;
-        const goods = await Goods.find({ category }).sort("-goodsId");
-        res.json({ goods: goods });
-    } catch (err) {
-        console.error(err);
-        next(err);
-    }
+router.get("/getajax", async (req, res, next) => {
+    res.send(req.body)
 });
 
-router.get("/goods/:goodsId", async (req, res) => {
-    const { goodsId } = req.params;
-    goods = await Goods.findOne({ goodsId: goodsId });
-    res.json({ detail: goods });
-});
+router.post('/postTest', (req, res, next) => {
+    console.log(req.body)
+
+})
 
 module.exports = router;
